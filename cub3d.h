dcf55a0af17d1ec9423411e4f7dc995dcd831b46
Katherine <01kathyy@gmail.com>
2022-09-19 21:58:24 +0900

    a

--- cub3d.h
+++ cub3d.h
@@ -1,10 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hejang <hejang@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/19 21:54:41 by hejang            #+#    #+#             */
+/*   Updated: 2022/09/19 21:58:18 by hejang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 #define CUB3D_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include <unistd.h>
+# include<fcntl.h>
+# include"./mlx/mlx.h"
 
 
 #endif
