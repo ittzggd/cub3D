de96d92ac9bcc47a6f1758d499fc5743c9c2d92c
Katherine <01kathyy@gmail.com>
2022-10-10 12:00:43 +0900

    a

---
+++ 221010/move.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hejang <hejang@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 20:25:55 by yukim             #+#    #+#             */
+/*   Updated: 2022/10/10 00:45:12 by hejang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./include/cub3d.h"
+
+void move_forward_or_backward(t_game *game)
+{
+	t_ray		*rray;
+	double		new_x;
+	double		new_y;
+
+	rray = &(game->ray);
+	if (game->key_flag.move_backward == TRUE)
+	{
+		new_x = rray->pl_x - rray->dir_x * rray->move_speed;
+		new_y = rray->pl_y - rray->dir_y * rray->move_speed;
+		game->key_flag.move_backward = FALSE;
+	}
+	else if (game->key_flag.move_forward == TRUE)
+	{
+		new_x = rray->pl_x + rray->dir_x * rray->move_speed;
+		new_y = rray->pl_y + rray->dir_y * rray->move_speed;
+		game->key_flag.move_forward = FALSE;
+	}
+	if ((game->map.maps[(int)new_x][(int)(rray->pl_y)] == '0') \
+			&& (game->map.maps[(int)(rray->pl_x)][(int)new_y] == '0'))
+	{
+		rray->pl_x = new_x;
+		rray->pl_y = new_y;
+	}
+}
+
+void move_left_or_right(t_game *game)
+{
+	t_ray		*rray;
+	double		new_x;
+	double		new_y;
+
+	if (game->key_flag.move_left == TRUE)
+	{
+		new_x = rray->pl_x + (rray->x_dir_x * rray->move_speed) + 0.00001; // 0.00001이 값 조절하면서 역할 체크
+		new_y = rray->pl_y + (rray->x_dir_y * rray->move_speed) + 0.00001;
+		game->key_flag.move_left = FALSE;
+	}
+	else if (game->key_flag.move_right == TRUE)
+	{
+		new_x = rray->pl_x - (rray->x_dir_x * rray->move_speed) + 0.00001;
+		new_y = rray->pl_y - (rray->x_dir_y * rray->move_speed) + 0.00001;
+		game->key_flag.move_right = FALSE;
+	}
+	if ((game->map.maps[(int)new_x][(int)(rray->pl_y)] == '0') \
+			&& (game->map.maps[(int)(rray->pl_x)][(int)new_y] == '0'))
+	{
+		rray->pl_x = new_x;
+		rray->pl_y = new_y;
+	}
+}
---
+++ 221010/utils/ft_str_func.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_func.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukim <yukim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/09 20:04:06 by yukim             #+#    #+#             */
+/*   Updated: 2022/10/09 20:04:06 by yukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int				i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while ((str1[i] != '\0' || str2[i] != '\0') && n > 0)
+	{
+		if (str1[i] != str2[i])
+			return (FALSE);
+		i++;
+		n--;
+	}
+	if (n == 0 || (str1[i] == '\0' && str2[i] == '\0'))
+		return (TRUE);
+	else
+		return (FALSE);
+}
+
+char	*ft_strdup(char *s)
+{
+	char	*ptr;
+	int		s_len;
+	int		i;
+
+	s_len = ft_strlen(s) + 1;
+	ptr = (char *)malloc(sizeof(char) * s_len);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*new;
+
+	i = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!new)
+		return (NULL);
+	while (i < (s1_len + s2_len))
+	{
+		if (i < s1_len)
+			new[i] = *(s1 + i);
+		else
+			new[i] = *(s2 + i - s1_len);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*save;
+
+	if (!s)
+		return (NULL);
+	save = NULL;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char) c)
+		{
+			save = (char *)s + i;
+			return (save);
+		}
+		i++;
+	}
+	if ((char )c == '\0')
+		save = (char *)s + i;
+	return (save);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	cnt;
+
+	cnt = 0;
+	if (s)
+	{
+		while (s[cnt] != '\0')
+			cnt++;
+	}
+	return (cnt);
+}
